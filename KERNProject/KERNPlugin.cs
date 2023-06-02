29cdf7fc0f4fd6d0ab3024205d77aa5b52412a0c
cvusmo <blacksheepcosmo@gmail.com>
2023-06-02 13:54:32 -0500

    **Added OnPreInitialize for addressables

--- KERNProject/KERNPlugin.cs
+++ KERNProject/KERNPlugin.cs
@@ -16,7 +16,6 @@ namespace KERN;
 [BepInDependency(SpaceWarpPlugin.ModGuid, SpaceWarpPlugin.ModVer)]
 public class KERNPlugin : BaseSpaceWarpPlugin
 {
-    // These are useful in case some other mod wants to add a dependency to this one
     public const string ModGuid = MyPluginInfo.PLUGIN_GUID;
     public const string ModName = MyPluginInfo.PLUGIN_NAME;
     public const string ModVer = MyPluginInfo.PLUGIN_VERSION;
@@ -27,11 +26,14 @@ public class KERNPlugin : BaseSpaceWarpPlugin
     private const string ToolbarFlightButtonID = "BTN-KERNFlight";
     private const string ToolbarOABButtonID = "BTN-KERNOAB";
 
+    public static string Path { get; private set; }
+
     public static KERNPlugin Instance { get; set; }
 
-    /// <summary>
-    /// Runs when the mod is first initialized.
-    /// </summary>
+    public void OnPreInitialize()
+    {
+        Path = PluginFolderPath;
+    }
     public override void OnInitialized()
     {
         base.OnInitialized();
@@ -62,35 +64,17 @@ public class KERNPlugin : BaseSpaceWarpPlugin
             }
         );
 
-        // Register all Harmony patches in the project
+
         Harmony.CreateAndPatchAll(typeof(KERNPlugin).Assembly);
-
-        // Try to get the currently active vessel, set its throttle to 100% and toggle on the landing gear
-        try
-        {
-            var currentVessel = Vehicle.ActiveVesselVehicle;
-            if (currentVessel != null)
-            {
-                currentVessel.SetMainThrottle(1.0f);
-                currentVessel.SetGearState(true);
-            }
-        }
-        catch (Exception e) {}
         
-        // Fetch a configuration value or create a default one if it does not exist
-        var defaultValue = "my_value";
-        var configValue = Config.Bind<string>("Settings section", "Option 1", defaultValue, "Option description");
+        var defaultValue = "KERN";
+        var configValue = Config.Bind<string>("Settings section", "Enabled", defaultValue, "Option description");
         
-        // Log the config value into <KSP2 Root>/BepInEx/LogOutput.log
-        Logger.LogInfo($"Option 1: {configValue.Value}");
+        Logger.LogInfo($"Enabled: {configValue.Value}");
     }
-
-    /// <summary>
-    /// Draws a simple UI window when <code>this._isWindowOpen</code> is set to <code>true</code>.
-    /// </summary>
     private void OnGUI()
     {
-        // Set the UI
+
         GUI.skin = Skins.ConsoleSkin;
 
         if (_isWindowOpen)
@@ -105,11 +89,6 @@ public class KERNPlugin : BaseSpaceWarpPlugin
             );
         }
     }
-
-    /// <summary>
-    /// Defines the content of the UI window drawn in the <code>OnGui</code> method.
-    /// </summary>
-    /// <param name="windowID"></param>
     private static void FillWindow(int windowID)
     {
         GUILayout.Label("KERN - Kerbin Council for Nuclear Research");
