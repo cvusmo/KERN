bee9e07de61eb241d5e511df83d0259f35419423
cvusmo <blacksheepcosmo@gmail.com>
2023-06-03 01:05:10 -0500

    **removed comments

--- KERNProject/KERNPlugin.cs
+++ KERNProject/KERNPlugin.cs
@@ -10,88 +10,90 @@ using SpaceWarp.API.UI;
 using SpaceWarp.API.UI.Appbar;
 using UnityEngine;
 
-namespace KERN;
-
-[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
-[BepInDependency(SpaceWarpPlugin.ModGuid, SpaceWarpPlugin.ModVer)]
-public class KERNPlugin : BaseSpaceWarpPlugin
+namespace KERN
 {
-    public const string ModGuid = MyPluginInfo.PLUGIN_GUID;
-    public const string ModName = MyPluginInfo.PLUGIN_NAME;
-    public const string ModVer = MyPluginInfo.PLUGIN_VERSION;
-    
-    private bool _isWindowOpen;
-    private Rect _windowRect;
 
-    private const string ToolbarFlightButtonID = "BTN-KERNFlight";
-    private const string ToolbarOABButtonID = "BTN-KERNOAB";
-
-    public static string Path { get; private set; }
-
-    public static KERNPlugin Instance { get; set; }
-
-    public void OnPreInitialize()
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    [BepInDependency(SpaceWarpPlugin.ModGuid, SpaceWarpPlugin.ModVer)]
+    public class KERNPlugin : BaseSpaceWarpPlugin
     {
-        Path = PluginFolderPath;
-    }
-    public override void OnInitialized()
-    {
-        base.OnInitialized();
+        public const string ModGuid = MyPluginInfo.PLUGIN_GUID;
+        public const string ModName = MyPluginInfo.PLUGIN_NAME;
+        public const string ModVer = MyPluginInfo.PLUGIN_VERSION;
 
-        Instance = this;
+        private bool _isWindowOpen;
+        private Rect _windowRect;
 
-        // Register Flight AppBar button
-        Appbar.RegisterAppButton(
-            "KERN",
-            ToolbarFlightButtonID,
-            AssetManager.GetAsset<Texture2D>($"{SpaceWarpMetadata.ModID}/images/icon.png"),
-            isOpen =>
-            {
-                _isWindowOpen = isOpen;
-                GameObject.Find(ToolbarFlightButtonID)?.GetComponent<UIValue_WriteBool_Toggle>()?.SetValue(isOpen);
-            }
-        );
+        private const string ToolbarFlightButtonID = "BTN-KERNFlight";
+        private const string ToolbarOABButtonID = "BTN-KERNOAB";
 
-        // Register OAB AppBar Button
-        Appbar.RegisterOABAppButton(
-            "KERN",
-            ToolbarOABButtonID,
-            AssetManager.GetAsset<Texture2D>($"{SpaceWarpMetadata.ModID}/images/icon.png"),
-            isOpen =>
-            {
-                _isWindowOpen = isOpen;
-                GameObject.Find(ToolbarOABButtonID)?.GetComponent<UIValue_WriteBool_Toggle>()?.SetValue(isOpen);
-            }
-        );
+        public static string Path { get; private set; }
 
+        public static KERNPlugin Instance { get; set; }
 
-        Harmony.CreateAndPatchAll(typeof(KERNPlugin).Assembly);
-        
-        var defaultValue = "KERN";
-        var configValue = Config.Bind<string>("Settings section", "Enabled", defaultValue, "Option description");
-        
-        Logger.LogInfo($"Enabled: {configValue.Value}");
-    }
-    private void OnGUI()
-    {
-
-        GUI.skin = Skins.ConsoleSkin;
-
-        if (_isWindowOpen)
+        public void OnPreInitialize()
         {
-            _windowRect = GUILayout.Window(
-                GUIUtility.GetControlID(FocusType.Passive),
-                _windowRect,
-                FillWindow,
-                "KERN",
-                GUILayout.Height(350),
-                GUILayout.Width(350)
-            );
+            Path = PluginFolderPath;
         }
-    }
-    private static void FillWindow(int windowID)
-    {
-        GUILayout.Label("KERN - Kerbin Council for Nuclear Research");
-        GUI.DragWindow(new Rect(0, 0, 10000, 500));
+        public override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            Instance = this;
+
+            // Register Flight AppBar button
+            Appbar.RegisterAppButton(
+                "KERN",
+                ToolbarFlightButtonID,
+                AssetManager.GetAsset<Texture2D>($"{SpaceWarpMetadata.ModID}/images/icon.png"),
+                isOpen =>
+                {
+                    _isWindowOpen = isOpen;
+                    GameObject.Find(ToolbarFlightButtonID)?.GetComponent<UIValue_WriteBool_Toggle>()?.SetValue(isOpen);
+                }
+            );
+
+            // Register OAB AppBar Button
+            Appbar.RegisterOABAppButton(
+                "KERN",
+                ToolbarOABButtonID,
+                AssetManager.GetAsset<Texture2D>($"{SpaceWarpMetadata.ModID}/images/icon.png"),
+                isOpen =>
+                {
+                    _isWindowOpen = isOpen;
+                    GameObject.Find(ToolbarOABButtonID)?.GetComponent<UIValue_WriteBool_Toggle>()?.SetValue(isOpen);
+                }
+            );
+
+
+            Harmony.CreateAndPatchAll(typeof(KERNPlugin).Assembly);
+
+            var defaultValue = "KERN";
+            var configValue = Config.Bind<string>("Settings section", "Enabled", defaultValue, "Option description");
+
+            Logger.LogInfo($"Enabled: {configValue.Value}");
+        }
+        private void OnGUI()
+        {
+
+            GUI.skin = Skins.ConsoleSkin;
+
+            if (_isWindowOpen)
+            {
+                _windowRect = GUILayout.Window(
+                    GUIUtility.GetControlID(FocusType.Passive),
+                    _windowRect,
+                    FillWindow,
+                    "KERN",
+                    GUILayout.Height(350),
+                    GUILayout.Width(350)
+                );
+            }
+        }
+        private static void FillWindow(int windowID)
+        {
+            GUILayout.Label("KERN - Kerbin Council for Nuclear Research");
+            GUI.DragWindow(new Rect(0, 0, 10000, 500));
+        }
     }
 }
