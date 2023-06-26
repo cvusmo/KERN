70d20ba8858305fb0f97330cb7614481ff6ec9b8
cvusmo <82072837+blacksheepcosmo@users.noreply.github.com>
2023-06-26 17:55:46 -0500

    Delete KERNProject/obj directory

--- KERNProject/obj/Debug/netstandard2.0/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace KERN
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "KERN";
-        public const string PLUGIN_NAME = "KERN";
-        public const string PLUGIN_VERSION = "0.1.0";
-    }
-}
